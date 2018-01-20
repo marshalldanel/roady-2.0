6126ae1a01747e297371a6bc861ba6840fcf1efc
Marshall Danel <marshalldanel@gmail.com>
2018-01-19 22:37:39 -0800

    Redux store init

---
+++ app/src/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import immutableCheckMiddleware from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+
+const middleware = process.env.NODE_ENV !== 'production' ?
+  [immutableCheckMiddleware(), thunk, createLogger()] :
+  [thunk];
+
+export default createStore(
+  rootReducer,
+  applyMiddleware(...middleware),
+);
