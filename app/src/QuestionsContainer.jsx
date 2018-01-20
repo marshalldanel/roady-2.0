575fdced80392865cb4da60babb90ab8f65f04f0
Marshall Danel <marshalldanel@gmail.com>
2018-01-19 22:42:46 -0800

    Update to pass props to GenreSelect component

--- app/src/QuestionsContainer.jsx
+++ app/src/QuestionsContainer.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addGenre, removeGenre } from './actions';
 import LocationList from './LocationList.jsx';
 import GenreSelect from './GenreSelect.jsx';
 
 class QuestionsContainer extends Component {
   render() {
+    const { genres, addGenre, removeGenre } = this.props;
     return (
       <div>
         <LocationList />
-        <GenreSelect />
+        <GenreSelect
+          genres={genres}
+          addGenre={addGenre}
+          removeGenre={removeGenre}
+        />
       </div>
     );
   }
 }
 
-export default QuestionsContainer;
+// Maps state from store to props
+const mapStateToProps = state => ({
+  genres: state.genres,
+});
+
+// Maps actions to props
+const mapDispatchToProps = dispatch => ({
+  addGenre: genre => dispatch(addGenre(genre)),
+  removeGenre: genre => dispatch(removeGenre(genre)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionsContainer);
