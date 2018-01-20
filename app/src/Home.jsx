93906685773e466eee0ac11230de3beecdc72787
Marshall Danel <marshalldanel@gmail.com>
2018-01-19 22:45:26 -0800

    Cond render components based on view + pass props to Button

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { updateView } from './actions';
 import QuestionsContainer from './QuestionsContainer.jsx';
+import Button from './Button.jsx';
 
 class Home extends Component {
   render() {
-    return (
-      <div className="section is-large">
-        <QuestionsContainer />
-      </div>
-    );
+    const { view, updateView } = this.props;
+
+    if (view === 'locations' || view === 'genres') {
+      return (
+        <div className="section is-large">
+          <QuestionsContainer />
+          <Button
+            view={view}
+            updateView={updateView}
+          />
+          {/* <CitiesResults />   */}
+
+        </div>
+      );
+    } else if (view === 'itinerary') {
+      return (
+        <div>
+          Hi
+        </div>
+      );
+    }
   }
 }
 
-export default Home;
+// Maps actions to props
+const mapDispatchToProps = dispatch => ({
+  updateView: view => dispatch(updateView(view)),
+});
+
+export default connect(null, mapDispatchToProps)(Home);
