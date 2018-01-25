a774b283174bbf2ecc1203493d78e41d43051810
Marshall Danel <marshalldanel@gmail.com>
2018-01-25 13:52:15 -0800

    Dynamically render app + update hot reload func

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
-import store from './store/configureStore';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 require('../styles/application.scss');
 
+const rootEl = document.getElementById('react-root');
+
+const render = () => {
+  // eslint-disable-next-line global-require
+  const App = require('./App').default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl,
+  );
+};
+
 if (module.hot) {
-  module.hot.accept();
+  console.log('client hot');
+  module.hot.accept('./App', () => {
+    // eslint-disable-next-line global-require
+    const nextApp = require('./App').default;
+    render(nextApp);
+  });
 }
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('react-root'),
-);
+render();
