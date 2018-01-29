051824315f2369b6ebe5ae91f70ade2f9c12344e
Marshall Danel <marshalldanel@gmail.com>
2018-01-28 18:45:44 -0800

    Add location action/reducers

--- app/src/store/actions/index.js
+++ app/src/store/actions/index.js
@@ -1,7 +1,10 @@
 export const UPDATE_VIEW = 'UPDATE_VIEW';
 export const ADD_GENRE = 'ADD_GENRE';
 export const REMOVE_GENRE = 'REMOVE_GENRE';
-
+export const SET_CITY = 'SET_CITY';
+export const SET_DATES = 'SET_DATES';
+export const ADD_LOCATION = 'ADD_LOCATION';
+export const REMOVE_LOCATION = 'REMOVE_LOCATION';
 
 export const updateView = view => ({
   type: 'UPDATE_VIEW',
@@ -16,4 +19,28 @@ export const addGenre = genre => ({
 export const removeGenre = genre => ({
   type: 'REMOVE_GENRE',
   genre,
+});
+
+export const setCity = (index, city) => ({
+  type: 'SET_CITY',
+  index,
+  city,
+});
+
+export const setDates = (index, startDate, endDate) => ({
+  type: 'SET_DATES',
+  index,
+  startDate,
+  endDate,
+});
+
+export const addLocation = (locations, index) => ({
+  type: 'ADD_LOCATION',
+  locations,
+  index,
+});
+
+export const removeLocation = index => ({
+  type: 'REMOVE_LOCATION',
+  index,
 });
--- app/src/store/reducers/index.js
+++ app/src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import ViewReducer from './reducer_view';
 import GenreReducer from './reducer_genres';
+import LocationReducer from './reducer_locations';
 
 const rootReducer = combineReducers({
   view: ViewReducer,
   genres: GenreReducer,
+  locations: LocationReducer,
 });
 
 export default rootReducer;
