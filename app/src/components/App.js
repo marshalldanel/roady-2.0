9b0ea9db39c6944071176bee6b246e963a15278a
Marshall Danel <marshalldanel@gmail.com>
2018-04-29 18:25:39 -0700

    Add ReactRouter and removed LocationList and QuestionContainer so Home is central component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import NavBar from './NavBar';
 import Home from './Home';
+import GenreSelect from './GenreSelect';
 
 class App extends Component {
   render() {
     return (
-      <div className="main-container">
-        <NavBar />
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="main-container">
+          <NavBar />
+          <Route path="/" component={Home} />
+          <Route exact path="/genres" component={GenreSelect} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import QuestionsContainer from './QuestionsContainer';
+import { Link } from 'react-router-dom';
+import LocationField from '../containers/LocationField';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <div className="section is-small" />
-        <QuestionsContainer />
+        <div className="locationsContainer container">
+          <h2 className="subtitle has-text-centered is-size-2 has-text-black-bis">Where are you travelling to?</h2>
+          <LocationField />
+        </div>
       </div>
     );
   }
